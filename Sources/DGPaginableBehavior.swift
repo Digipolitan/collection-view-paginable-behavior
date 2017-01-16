863ef4c7182d050d50e6ad3e233cb3564c447b95
Julien Sarazin <julien.sarazin@me.com>
2017-01-16 14:31:26 +0100

    added error parameter in completion callback to handle error during fetching requests

--- Sources/DGPaginableBehavior.swift
+++ Sources/DGPaginableBehavior.swift
@@ -11,7 +11,7 @@ import UIKit
 @objc
 public protocol DGPaginableBehaviorDelegate: UICollectionViewDelegateFlowLayout {
 	@objc optional func paginableBehavior(_ paginableBehavior: DGPaginableBehavior, countPerPageInSection section: Int) -> Int
-	@objc optional func paginableBehavior(_ paginableBehavior: DGPaginableBehavior, fetchDataFrom indexPath: IndexPath, with count: Int, completion: @escaping (Int) -> Void)
+	@objc optional func paginableBehavior(_ paginableBehavior: DGPaginableBehavior, fetchDataFrom indexPath: IndexPath, with count: Int, completion: @escaping (Error?, Int) -> Void)
 }
 
 
@@ -21,7 +21,7 @@ public enum DGPaginableBehaviorOptions: String {
 }
 
 open class DGPaginableBehavior: NSObject {
-	fileprivate(set) public var sectionInfo: [Int: (fetching: Bool, done: Bool)]
+	fileprivate(set) public var sectionInfo: [Int: (fetching: Bool, done: Bool, error: Error?)]
 	fileprivate(set) var options: [DGPaginableBehaviorOptions: Any]
 	fileprivate var lastIndexes: [Int: Int]
 
@@ -29,7 +29,7 @@ open class DGPaginableBehavior: NSObject {
 
 	public override init() {
 		self.lastIndexes = [Int: Int]()
-		self.sectionInfo = [Int: (fetching: Bool, done: Bool)]()
+		self.sectionInfo = [Int: (fetching: Bool, done: Bool, error: Error?)]()
 		self.options = [
 			.automaticFetch: true,
 			.countPerPage: 10
@@ -54,8 +54,8 @@ open class DGPaginableBehavior: NSObject {
 		return nil
 	}
 
-	public func isDoneLoading(section: Int) -> Bool {
-		return self.sectionInfo[section]?.done ?? false
+	public func statusesForSection(_ section: Int) -> (fetching: Bool, done: Bool, error: Error?) {
+		return self.sectionInfo[section] ?? (fetching: false, done: false, error: nil)
 	}
 
 	public func set(options: [DGPaginableBehaviorOptions: Any]) {
@@ -73,7 +73,7 @@ open class DGPaginableBehavior: NSObject {
 		var loadingInfo = self.sectionInfo[section]
 
 		if loadingInfo == nil {
-			loadingInfo = (fetching: false, done: false)
+			loadingInfo = (fetching: false, done: false, error: nil)
 			self.sectionInfo[section] = loadingInfo
 		}
 
@@ -83,16 +83,20 @@ open class DGPaginableBehavior: NSObject {
 
 		loadingInfo!.fetching = true
 		self.sectionInfo[section] = loadingInfo
-		self.lastIndexes[section] = index! + count
-
 
 		self.delegate?.paginableBehavior?(self,
 		                                  fetchDataFrom: IndexPath(item:index!, section: section),
 		                                  with: count,
-		                                  completion: { (elementsCount) in
+		                                  completion: { (error, elementsCount) in
+											if error == nil {
+												loadingInfo!.done = (elementsCount == 0 || elementsCount < count)
+												self.lastIndexes[section] = index! + count
+											}
+
+											loadingInfo!.error = error
 											loadingInfo!.fetching = false
-											loadingInfo!.done = (elementsCount == 0 || elementsCount < count)
 											self.sectionInfo[section] = loadingInfo
+
 											if (done != nil) {
 												done();
 											}
@@ -101,7 +105,6 @@ open class DGPaginableBehavior: NSObject {
 }
 
 extension DGPaginableBehavior: UICollectionViewDelegateFlowLayout {
-
 	public func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
 		self.delegate?.collectionView?(collectionView, willDisplay: cell, forItemAt: indexPath)
 
