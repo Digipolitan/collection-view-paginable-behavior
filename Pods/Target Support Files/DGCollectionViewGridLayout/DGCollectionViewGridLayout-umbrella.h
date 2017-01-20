7fb49c722ef72c0e466bd7e37dd39fc00eb0ec78
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2017-01-20 10:40:39 +0100

    remove Pods/ from tracking

--- Pods/Target Support Files/DGCollectionViewGridLayout/DGCollectionViewGridLayout-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "DGCollectionViewGridLayout.h"
-
-FOUNDATION_EXPORT double DGCollectionViewGridLayoutVersionNumber;
-FOUNDATION_EXPORT const unsigned char DGCollectionViewGridLayoutVersionString[];
-
