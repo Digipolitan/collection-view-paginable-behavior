804d2b3582a5d75c66d7433f4e68d4112a9697e4
Julien Sarazin <julien.sarazin@me.com>
2017-02-10 15:05:30 +0100

    updated Samples

--- Samples/DGCollectionViewPaginableBehaviorSample-iOS/DGCollectionViewPaginableBehaviorSample-iOS/UserItemCell.swift
+++ Samples/DGCollectionViewPaginableBehaviorSample-iOS/DGCollectionViewPaginableBehaviorSample-iOS/UserItemCell.swift
@@ -22,5 +22,13 @@ class UserItemCell: UICollectionViewCell {
 	func set(user: User) {
 		self.nameLabel.text = "\(user.firstName) \(user.lastName)"
 		self.companyLabel.text = user.company
+		let htmlString = "<h1>Header</h1><h2>Subheader</h2><p>Some <em>text</em></p><img src='http://68.media.tumblr.com/c06b9a1fb051899055827ee8c7503cef/tumblr_ol2c7iyO261v1wvcuo1_1280.jpg' width=70 height=100 />"
+		do {
+			let attributedString = try NSAttributedString(data: htmlString.data(using: .unicode, allowLossyConversion: true)!,
+			                                              options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType], documentAttributes: nil)
+			self.nameLabel.attributedText = attributedString
+		}
+		catch {}
+
 	}
 }
